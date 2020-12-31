bba23e749ec7e31a7f569da6cdfabc57c1fa63ef
BenSaus <jonas26a@gmail.com>
2020-12-31 11:40:22 -0700

    Cleaning up group list

--- src/components/GroupList/GroupList.tsx
+++ src/components/GroupList/GroupList.tsx
@@ -7,11 +7,6 @@ import {
     Avatar,
     Chip,
     IconButton,
-    List,
-    ListItem,
-    ListItemAvatar,
-    ListItemSecondaryAction,
-    ListItemText,
     Table,
     TableBody,
     TableCell,
@@ -19,6 +14,7 @@ import {
     TableHead,
     TableRow,
     Tooltip,
+    Typography,
 } from "@material-ui/core"
 import { Delete, FindInPage, GroupAddRounded } from "@material-ui/icons"
 
@@ -42,69 +38,45 @@ interface GroupListProps {
 const GroupList: React.FC<GroupListProps> = (props) => {
     const classes = useStyles()
 
-    // let groupsJsx
-    // if (props.groups) {
-    //     groupsJsx = props.groups.map((group: Group) => {
-    //         if (
-    //             props.showGroupsWithNoOpenSlots ||
-    //             groupUtil.getOpenSlots(group) > 0
-    //         ) {
-    //             return (
-    //                 <GroupComponent
-    //                     group={group}
-    //                     clicked={props.clickedGroup}
-    //                     key={group.id}
-    //                 />
-    //             )
-    //         }
-    //     })
-    // }
-
-    // return <div className={classes.groupList}>{groupsJsx}</div>
-
-    // return (
-    //     <List>
-    //         {props.groups.map((group) => (
-    //             <ListItem button alignItems="flex-start" key={group.id}>
-    //                 <ListItemAvatar>
-    //                     <GroupAddRounded />
-    //                 </ListItemAvatar>
-    //                 <ListItemText>{group.name}</ListItemText>
-    //                 <ListItemText>
-    //                     Ages: {group.min_age} - {group.max_age}
-    //                 </ListItemText>
-    //                 <ListItemSecondaryAction>
-    //                     <IconButton>
-    //                         <FindInPage />
-    //                     </IconButton>
-    //                 </ListItemSecondaryAction>
-    //             </ListItem>
-    //         ))}
-    //     </List>
-    // )
-
-    // Handlers
-
-    // const groupClickHandler: (event: any) {
-    //     props.clickedGroup(group.id)
-    // }
-
     return (
         <TableContainer>
             <Table>
                 <TableHead>
                     <TableRow>
-                        <TableCell>Group Name</TableCell>
-                        <TableCell>Leader</TableCell>
-                        <TableCell align="center">Age Range</TableCell>
-                        <TableCell align="center">Open Slots</TableCell>
-                        <TableCell align="center">Actions</TableCell>
+                        <TableCell style={{ fontWeight: "bold" }}>
+                            Group Name
+                        </TableCell>
+                        <TableCell style={{ fontWeight: "bold" }}>
+                            Leader
+                        </TableCell>
+                        <TableCell
+                            style={{ fontWeight: "bold" }}
+                            align="center"
+                        >
+                            Age Range
+                        </TableCell>
+                        <TableCell
+                            style={{ fontWeight: "bold" }}
+                            align="center"
+                        >
+                            Open Slots
+                        </TableCell>
+                        <TableCell
+                            style={{ fontWeight: "bold" }}
+                            align="center"
+                        >
+                            Actions
+                        </TableCell>
                     </TableRow>
                 </TableHead>
                 <TableBody>
                     {props.groups.map((group) => (
                         <TableRow key={group.id}>
-                            <TableCell>{group.name}</TableCell>
+                            <TableCell>
+                                <Typography variant="h6">
+                                    {group.name}
+                                </Typography>
+                            </TableCell>
                             <TableCell component="th" scope="row">
                                 <div className={classes.userCell}>
                                     <Avatar
